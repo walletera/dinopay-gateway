2b2f263ce24fcbf67a072c39a90e262b9ae20ae7
Federico Moya <federicoamoya@gmail.com>
2024-02-16 10:43:14 -0300

    add dummy implementations for the step functions and add a ScenarioInitializer to the TestSuite

--- internal/tests/process_withdrawal_created_test.go
+++ internal/tests/process_withdrawal_created_test.go
@@ -9,6 +9,7 @@ import (
 func TestFeatures(t *testing.T) {
 
     suite := godog.TestSuite{
+        ScenarioInitializer: InitializeScenario,
         Options: &godog.Options{
             Format:   "pretty",
             Paths:    []string{"features"},
@@ -19,4 +20,32 @@ func TestFeatures(t *testing.T) {
     if suite.Run() != 0 {
         t.Fatal("non-zero status returned, failed to run feature tests")
     }
+}
+
+func InitializeScenario(ctx *godog.ScenarioContext) {
+    ctx.Given(`^a withdrawal created event:$`, aWithdrawalCreatedEvent)
+    ctx.Given(`^a dinopay endpoint to create payments:$`, aDinopayEndpointToCreatePayments)
+    ctx.When(`^the event is published$`, theEventIsPublished)
+    ctx.Then(`^the dinopay-gateway creates the corresponding payment on the DinoPay API$`, theDinopaygatewayCreatesTheCorrespondingPaymentOnTheDinoPayAPI)
+    ctx.Then(`^the dinopay-gateway produces the following log:$`, theDinopaygatewayProducesTheFollowingLog)
+}
+
+func aWithdrawalCreatedEvent(arg1 *godog.DocString) error {
+    return godog.ErrPending
+}
+
+func aDinopayEndpointToCreatePayments(arg1 *godog.DocString) error {
+    return godog.ErrPending
+}
+
+func theEventIsPublished() error {
+    return godog.ErrPending
+}
+
+func theDinopaygatewayCreatesTheCorrespondingPaymentOnTheDinoPayAPI() error {
+    return godog.ErrPending
+}
+
+func theDinopaygatewayProducesTheFollowingLog(arg1 *godog.DocString) error {
+    return godog.ErrPending
 }
