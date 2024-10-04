1e62e71237debb266edb53a98040b6f060a40d3d
Federico Moya <federicoamoya@gmail.com>
2024-10-04 15:16:31 -0300

    substitute the term visitor by handler

--- internal/domain/events/dinopay/events.go
+++ internal/domain/events/dinopay/events.go
@@ -1,55 +1,55 @@
 package dinopay
 
 import (
-    "context"
-    "encoding/json"
-    "time"
+	"context"
+	"encoding/json"
+	"time"
 
-    "github.com/google/uuid"
-    "github.com/walletera/message-processor/errors"
+	"github.com/google/uuid"
+	"github.com/walletera/message-processor/errors"
 )
 
 type PaymentCreated struct {
-    Id        uuid.UUID   `json:"id"`
-    EventType string      `json:"type"`
-    Time      time.Time   `json:"time"`
-    Data      PaymentData `json:"data"`
+	Id        uuid.UUID   `json:"id"`
+	EventType string      `json:"type"`
+	Time      time.Time   `json:"time"`
+	Data      PaymentData `json:"data"`
 }
 
 type PaymentData struct {
-    Id                 uuid.UUID `json:"id"`
-    Amount             int       `json:"amount"`
-    Currency           string    `json:"currency"`
-    SourceAccount      Account   `json:"sourceAccount"`
-    DestinationAccount Account   `json:"destinationAccount"`
+	Id                 uuid.UUID `json:"id"`
+	Amount             int       `json:"amount"`
+	Currency           string    `json:"currency"`
+	SourceAccount      Account   `json:"sourceAccount"`
+	DestinationAccount Account   `json:"destinationAccount"`
 }
 
 type Account struct {
-    AccountHolder string `json:"accountHolder"`
-    AccountNumber string `json:"accountNumber"`
+	AccountHolder string `json:"accountHolder"`
+	AccountNumber string `json:"accountNumber"`
 }
 
 func (p PaymentCreated) ID() string {
-    return p.Id.String()
+	return p.Id.String()
 }
 
 func (p PaymentCreated) Type() string {
-    return p.EventType
+	return p.EventType
 }
 
 func (p PaymentCreated) CorrelationID() string {
-    //TODO implement me
-    panic("implement me")
+	//TODO implement me
+	panic("implement me")
 }
 
 func (p PaymentCreated) DataContentType() string {
-    return "application/json"
+	return "application/json"
 }
 
 func (p PaymentCreated) Serialize() ([]byte, error) {
-    return json.Marshal(p)
+	return json.Marshal(p)
 }
 
-func (p PaymentCreated) Accept(ctx context.Context, visitor EventsVisitor) errors.ProcessingError {
-    return visitor.VisitPaymentCreated(ctx, p)
+func (p PaymentCreated) Accept(ctx context.Context, handler EventsHandler) errors.ProcessingError {
+	return handler.VisitPaymentCreated(ctx, p)
 }
--- internal/domain/events/walletera/gateway/outbound/payment_created_event.go
+++ internal/domain/events/walletera/gateway/outbound/payment_created_event.go
@@ -1,54 +1,54 @@
 package outbound
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
+	"context"
+	"encoding/json"
+	"fmt"
 
-    "github.com/google/uuid"
-    "github.com/walletera/dinopay-gateway/internal/domain/events/walletera/gateway"
-    "github.com/walletera/message-processor/errors"
-    "github.com/walletera/message-processor/events"
+	"github.com/google/uuid"
+	"github.com/walletera/dinopay-gateway/internal/domain/events/walletera/gateway"
+	"github.com/walletera/message-processor/errors"
+	"github.com/walletera/message-processor/events"
 )
 
-var _ events.Event[EventsVisitor] = PaymentCreated{}
+var _ events.Event[EventsHandler] = PaymentCreated{}
 
 type PaymentCreated struct {
-    Id                   uuid.UUID `json:"id,omitempty"`
-    WithdrawalId         uuid.UUID `json:"withdrawal_id,omitempty"`
-    DinopayPaymentId     uuid.UUID `json:"dinopay_payment_id,omitempty"`
-    DinopayPaymentStatus string    `json:"dinopay_payment_status,omitempty"`
-    CreatedAt            int64     `json:"created_at,omitempty"`
+	Id                   uuid.UUID `json:"id,omitempty"`
+	WithdrawalId         uuid.UUID `json:"withdrawal_id,omitempty"`
+	DinopayPaymentId     uuid.UUID `json:"dinopay_payment_id,omitempty"`
+	DinopayPaymentStatus string    `json:"dinopay_payment_status,omitempty"`
+	CreatedAt            int64     `json:"created_at,omitempty"`
 }
 
 func (o PaymentCreated) ID() string {
-    return fmt.Sprintf("%s-%s", o.Type(), o.Id)
+	return fmt.Sprintf("%s-%s", o.Type(), o.Id)
 }
 
 func (o PaymentCreated) Type() string {
-    return "OutboundPaymentCreated"
+	return "OutboundPaymentCreated"
 }
 
 func (o PaymentCreated) DataContentType() string {
-    return "application/json"
+	return "application/json"
 }
 
 func (o PaymentCreated) CorrelationID() string {
-    panic("not implemented yet")
+	panic("not implemented yet")
 }
 
-func (o PaymentCreated) Accept(ctx context.Context, visitor EventsVisitor) errors.ProcessingError {
-    return visitor.VisitOutboundPaymentCreated(ctx, o)
+func (o PaymentCreated) Accept(ctx context.Context, handler EventsHandler) errors.ProcessingError {
+	return handler.VisitOutboundPaymentCreated(ctx, o)
 }
 
 func (o PaymentCreated) Serialize() ([]byte, error) {
-    data, err := json.Marshal(o)
-    if err != nil {
-        return nil, fmt.Errorf("failed serializing OutbounPaymentCreated event: %w", err)
-    }
-    envelope := gateway.EventEnvelope{
-        Type: "OutboundPaymentCreated",
-        Data: data,
-    }
-    return json.Marshal(envelope)
+	data, err := json.Marshal(o)
+	if err != nil {
+		return nil, fmt.Errorf("failed serializing OutbounPaymentCreated event: %w", err)
+	}
+	envelope := gateway.EventEnvelope{
+		Type: "OutboundPaymentCreated",
+		Data: data,
+	}
+	return json.Marshal(envelope)
 }
